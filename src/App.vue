df020bc7537aeaa84fce9608ab8b6d85e34124e7
PAnsah98 <portiaansah15@gmail.com>
2021-07-20 10:37:46 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <show-button :loading="loading">I am awesome</show-button>
+  </div>
+</template>
+
+<script>
+import showButton from './showButton.vue';
+
+export default {
+  components: {
+      'show-button': showButton
+  },
+ 
+  data () {
+    return {
+      loading: "Loading..."
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/showButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="show-button">
+   <button v-bind:class="{'red':clicked, 'blue':clicked}" v-on:click="clicked = !clicked">{{loading}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['loading'],
+    data(){
+        return{
+      clicked: true
+    }
+    },
+    methods: {
+ 
+    }
+    
+}
+</script>
+
+<style>
+.red {
+    background-color: red;
+    width: 200px;
+    height: 20px;
+}
+
+.blue {
+    background-color: blue;
+    width: 100px;
+    height: 30px;
+    color: white;
+}
+
+</style>
