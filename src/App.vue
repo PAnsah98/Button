2082cf95e8946d005231657e567e11fea7aca6b7
PAnsah98 <portiaansah15@gmail.com>
2021-07-25 14:43:20 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
   <div id="app">
-    <show-button :loading="loading">I am awesome</show-button>
+    <show-button
+      :backgroundColor="backgroundColor"
+      size="small"
+      :text="buttonText"
+      @click.native="doSomething"
+      >I am awesome</show-button
+    >
   </div>
 </template>
 
 <script>
-import showButton from './showButton.vue';
+import showButton from "./showButton.vue";
 
 export default {
   components: {
-      'show-button': showButton
+    "show-button": showButton,
   },
- 
-  data () {
+
+  data() {
     return {
-      loading: "Loading..."
-    
-    }
-  }
-}
+      loading: false,
+      backgroundColor: "#eee",
+      buttonText: 'Click Me'
+    };
+  },
+
+  computed: {
+    showLoading() {
+      return this.loading;
+    },
+  },
+
+  methods: {
+    simulateAsyncFunction: function () {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const randomNum = Math.floor(Math.random() * 2) + 1;
+          if (randomNum === 1) resolve("success");
+          else reject("failed");
+        }, 1000);
+      });
+    },
+    doSomething: function () {
+      this.buttonText = 'loading...'
+
+      // reset the background color to the default anytime u are making a request
+      this.backgroundColor = '#eee'
+      
+      this.simulateAsyncFunction()
+        .then((res) => {
+          this.backgroundColor = "green";
+          this.buttonText = 'success'
+        })
+        .catch((error) => {
+          this.backgroundColor = "red"
+          this.buttonText = 'failed, retry'
+        })
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
--- src/showButton.vue
+++ src/showButton.vue
@@ -1,36 +1,45 @@
 <template>
-  <div id="show-button">
-   <button v-bind:class="{'red':clicked, 'blue':clicked}" v-on:click="clicked = !clicked">{{loading}}</button>
-    </div>
+  <button :style="buttonStyle" style="padding: 4px 12px">
+    <span>{{ text}} </span>
+  </button>
 </template>
 
 <script>
+const sizes = ["small", "medium", "large"];
+
+const buttonStyles = [
+  { name: "small", styles: { height: "40px" } },
+  { name: "medium", styles: { height: "80px" } },
+  { name: "large", styles: { height: "100px" } },
+];
 export default {
-    props: ['loading'],
-    data(){
-        return{
-      clicked: true
-    }
+  props: {
+    text: {
+    type : String,
     },
-    methods: {
- 
-    }
-    
-}
+    size: {
+      type: String,
+      default: "medium",
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  computed: {
+    buttonSize() {
+      let validSize = sizes.some((item) => {
+        return item === this.size;
+      });
+      return validSize ? this.size : "small";
+    },
+
+    buttonStyle() {
+      let style = buttonStyles.filter((a) => {
+        return a.name === this.buttonSize;
+      });
+      return { ...style[0].styles, backgroundColor: this.backgroundColor };
+    },
+  },
+};
 </script>
-
-<style>
-.red {
-    background-color: red;
-    width: 200px;
-    height: 20px;
-}
-
-.blue {
-    background-color: blue;
-    width: 100px;
-    height: 30px;
-    color: white;
-}
-
-</style>
